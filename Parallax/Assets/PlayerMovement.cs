494fb53a062e56ea6d1e15951568e7611f56e496
MilanMcI <mcinerneymilan@gmail.com>
2026-03-16 18:44:09 +0000

    qol changes
    improved tilemap selection, adjusted penguin collider, cleaned up project files

---
+++ Parallax/Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float move = 0f;
+
+        if (Input.GetKey(KeyCode.A))
+            move = -moveSpeed;
+
+        if (Input.GetKey(KeyCode.D))
+            move = moveSpeed;
+
+        rb.linearVelocity = new Vector2(move, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
